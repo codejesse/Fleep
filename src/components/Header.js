d2ca8a5555285f95452ca5536cb73d8a8bd4ab84
Jesse <jessebeke10@gmail.com>
2023-08-30 01:14:09 +0100

    Background theme fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <nav class="bg-white fixed w-full z-20 top-0 left-0 border-b border-gray-200">
+    <nav class="bg-white fixed w-full z-20 top-0 left-0">
       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="https://flowbite.com/" class="flex items-center">
           <span class="self-center text-2xl font-semibold whitespace-nowrap">FLEEP</span>
