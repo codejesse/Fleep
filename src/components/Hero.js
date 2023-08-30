ca7480cafcbc286b8db974d1d789d2d60fa3113e
Jesse <jessebeke10@gmail.com>
2023-08-30 01:51:49 +0100

    Hero UI setup --2

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,7 +4,11 @@ import heroimg from '../assets/img/hero-image.png'
 const Hero = () => {
   return (
     <div className='flex flex-col lg:flex-row overflow-hidden mt-[100px]'>
-      <h1 className='border w-[620px] m-auto font-semibold text-[70px]'>Flip Your Dream House With Us</h1>
+      <div className='flex flex-col m-auto w-[620px]'>
+        <h1 className='font-semibold text-[70px]'>Flip Your Dream House With Us</h1>
+        <p>Powerful, self-serve product and growth analytics to help
+          you flip homes and resell to the market.</p>
+      </div>
       <img className='w-[45%]' src={heroimg} />
     </div>
   );
