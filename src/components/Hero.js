2d631ebe4f61e9368d34af022d114179b8d30469
Jesse <jessebeke10@gmail.com>
2023-08-30 01:52:35 +0100

    Hero UI setup --3

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,7 +6,7 @@ const Hero = () => {
     <div className='flex flex-col lg:flex-row overflow-hidden mt-[100px]'>
       <div className='flex flex-col m-auto w-[620px]'>
         <h1 className='font-semibold text-[70px]'>Flip Your Dream House With Us</h1>
-        <p>Powerful, self-serve product and growth analytics to help
+        <p className='text-[#848484]'>Powerful, self-serve product and growth analytics to help
           you flip homes and resell to the market.</p>
       </div>
       <img className='w-[45%]' src={heroimg} />
