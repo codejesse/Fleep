f4cd1e310845ebb137ffd40ea0740e98ec0b4dc6
Jesse <jessebeke10@gmail.com>
2023-08-30 01:42:07 +0100

    Hero first commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import heroimg from '../assets/img/hero-image.png'
 
 const Hero = () => {
-  return <div>Banner</div>;
+  return (
+    <div className='flex flex-row overflow-hidden mt-[100px]'>
+      <h1 className='border m-auto font-semibold text-[45px]'>Flip Your Dream House With Us</h1>
+      <img className='w-[50%]' src={heroimg} />
+    </div>
+  );
 };
 
 export default Hero;
