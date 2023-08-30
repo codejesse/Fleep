8e67410905fc38575e7e5ed46c7c93a036eb019e
Jesse <jessebeke10@gmail.com>
2023-08-30 01:11:25 +0100

    Navbar component fixes --2

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,6 +8,7 @@ const Header = () => {
           <span class="self-center text-2xl font-semibold whitespace-nowrap">FLEEP</span>
         </a>
         <div class="flex md:order-2">
+              <a href="#" class="block py-2 pr-3 text-[#252525]">Login</a>
           <button type="button" class="text-white bg-[#252525] hover:bg-[#252525] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium border-none text-sm px-5 py-2 text-center mr-3 md:mr-0">Sign Up</button>
           <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
             <span class="sr-only">Open main menu</span>
@@ -17,18 +18,18 @@ const Header = () => {
           </button>
         </div>
         <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-          <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white">
-            <li>
+          <ul class="flex flex-col p-4 md:p-0 mt-4 ml-10 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white">
+            {/* <li>
               <a href="#" class="block py-2 pl-3 pr-4 text-[#252525]" aria-current="page">Home</a>
+            </li> */}
+            <li>
+              <a href="#" class="block py-2 pl-3 pr-4 text-[#252525]">Homes</a>
             </li>
             <li>
-              <a href="#" class="block py-2 pl-3 pr-4 text-[#252525]">About</a>
+              <a href="#" class="block py-2 pl-3 pr-4 text-[#252525]">Locations</a>
             </li>
             <li>
-              <a href="#" class="block py-2 pl-3 pr-4 text-[#252525]">Services</a>
-            </li>
-            <li>
-              <a href="#" class="block py-2 pl-3 pr-4 text-[#252525]">Contact</a>
+              <a href="#" class="block py-2 pl-3 pr-4 text-[#252525]">How it works</a>
             </li>
           </ul>
         </div>
