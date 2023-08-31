c2cd9b71cddecb41c6a3c32c0f3e27977b6f2312
Jesse <jessebeke10@gmail.com>
2023-09-01 00:37:11 +0100

    Search area ui setup

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Search = () => {
   return (
     <div className='m-auto overflow-hidden mt-[-10px] z-[999px]'>
-      <div className='m-auto w-[90%] border z-[999px]'>
+      <div className='m-auto w-[90%] border z-[999px] p-8 h-full bg-white rounded-lg bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border-gray-100'>
         Search
       </div>
     </div>
