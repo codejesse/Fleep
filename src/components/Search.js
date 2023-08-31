48878e3e84b795c256f19b6981293fcc52bd78b0
Jesse <jessebeke10@gmail.com>
2023-09-01 00:50:07 +0100

    Search area ui setup --3

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Search = () => {
   return (
-    <div className='m-auto overflow-hidden mt-[-10px] z-[999px]'>
-      <div className='m-auto w-[90%] z-[999px] p-8 h-full bg-[rgba(255, 255, 255, 0.50)] rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10'>
-        <input placeholder='Locations (any)' className='bg-white border border-[#ECECEC] rounded-xl w-[15%] p-4' />
+    <div className='m-auto overflow-hidden mt-[-20px] z-[999px]'>
+      <div className='flex gap-10 m-auto w-[90%] z-[999px] p-8 h-full bg-[rgba(255, 255, 255, 0.50)] rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-10'>
+        <input placeholder='Locations (any)' className='bg-white border border-[#ECECEC] rounded-2xl w-[25%] p-5' />
+        <input placeholder='Locations (any)' className='bg-white border border-[#ECECEC] rounded-2xl w-[25%] p-5' />
+        <input placeholder='Locations (any)' className='bg-white border border-[#ECECEC] rounded-2xl w-[25%] p-5' />
       </div>
     </div>
   );
