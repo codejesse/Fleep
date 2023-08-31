eb6728d61c0cdcedbccd39c94587076bedd71b79
Jesse <jessebeke10@gmail.com>
2023-08-31 21:24:11 +0100

    Hero UI setup --6

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Search from './components/Search';
 
 const App = () => {
   return (
     <>
     <Header />
     <Hero />
+    <Search />
     </>
   )
 };
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <div className='m-auto overflow-hidden mt-[-10px] z-[999px]'>
+      <div className='m-auto w-[90%] border z-[999px]'>
+        Search
+      </div>
+    </div>
+  );
 };
 
 export default Search;
