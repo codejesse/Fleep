0ad8bd49d9d14f7abf4fad56327602fbb38ce26f
Jesse <jessebeke10@gmail.com>
2023-11-03 11:30:30 +0100

    Home details text Ui - disclosure edit (8)

--- src/pages/PropertyDetails.js
+++ src/pages/PropertyDetails.js
@@ -35,11 +35,11 @@ const PropertyDetails = () => {
             <Disclosure>
               {({ open }) => (
                 <>
-                  <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500/75">
+                  <Disclosure.Button className="flex w-full justify-between rounded-lg bg-[#000000] px-4 py-2 text-left text-sm font-medium text-white">
                     <span>Description of property</span>
                     <ChevronUpIcon
                       className={`${open ? 'rotate-180 transform' : ''
-                        } h-5 w-5 text-purple-500`}
+                        } h-5 w-5 text-white`}
                     />
                   </Disclosure.Button>
                   <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
@@ -52,11 +52,11 @@ const PropertyDetails = () => {
             <Disclosure as="div" className="mt-2">
               {({ open }) => (
                 <>
-                  <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500/75">
+                  <Disclosure.Button className="flex w-full justify-between rounded-lg bg-[#000000] px-4 py-2 text-left text-sm font-medium text-white">
                     <span>Location and survery</span>
                     <ChevronUpIcon
                       className={`${open ? 'rotate-180 transform' : ''
-                        } h-5 w-5 text-purple-500`}
+                        } h-5 w-5 text-white`}
                     />
                   </Disclosure.Button>
                   <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
@@ -68,11 +68,11 @@ const PropertyDetails = () => {
             <Disclosure as="div" className="mt-2">
               {({ open }) => (
                 <>
-                  <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500/75">
+                  <Disclosure.Button className="flex w-full justify-between rounded-lg bg-[#000000] px-4 py-2 text-left text-sm font-medium text-white">
                     <span>Legal</span>
                     <ChevronUpIcon
                       className={`${open ? 'rotate-180 transform' : ''
-                        } h-5 w-5 text-purple-500`}
+                        } h-5 w-5 text-white`}
                     />
                   </Disclosure.Button>
                   <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
