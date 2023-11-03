5a6b7f5bf25f01382b392f1719ee2e8747039b47
Jesse <jessebeke10@gmail.com>
2023-11-03 10:43:03 +0100

    Cleaning somethings

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import React from 'react';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import Search from './components/Search';
-import Footer from './components/Footer';
 import { Route, Link, Routes } from 'react-router-dom';
 import PropertyDetails from './pages/PropertyDetails';
 import Home from './pages/Home';
@@ -11,10 +7,6 @@ import Home from './pages/Home';
 const App = () => {
   return (
     <>
-      {/* <Header />
-      <Hero />
-      <Search />
-      <Footer /> */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="homes/:homeId" element={<PropertyDetails />} />
